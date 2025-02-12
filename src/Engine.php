26099adc2c87b447368b3a92ba08273fc0a5c70f
Roman Kostrov <rkostrov@yandex.ru>
2025-02-12 20:35:57 +0300

    logic

--- src/Engine.php
+++ src/Engine.php
@@ -3,3 +3,45 @@
 namespace BrainGames\Engine;
 
 require __DIR__ . '/../vendor/autoload.php'; // Подключаем автозагрузчик Composer
+
+<?php
+
+namespace BrainGames\\Engine;
+
+use function cli\\line;
+use function cli\\prompt;
+
+const ROUNDS_COUNT = 3;
+
+/**
+ * Общая логика для запуска игр.
+ *
+ * @param string $description Описание игры.
+ * @param callable $getRoundData Функция, возвращающая данные для одного раунда игры (вопрос и правильный ответ).
+ *
+ * @return void
+ */
+function runGame(string $description, callable $getRoundData): void
+{
+    line("Welcome to the Brain Games!");
+    $userName = prompt("May I have your name?");
+    line("Hello, %s!", $userName);
+    line($description);
+
+    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
+        ['question' => $question, 'correctAnswer' => $correctAnswer] = $getRoundData();
+
+        line("Question: %s", $question);
+        $userAnswer = prompt("Your answer");
+
+        if ($userAnswer == $correctAnswer) {
+            line("Correct!");
+        } else {
+            line("'%s' is wrong answer ;(. Correct answer was '%s'.", $userAnswer, $correctAnswer);
+            line("Let's try again, %s!", $userName);
+            return;
+        }
+    }
+
+    line("Congratulations, %s!", $userName);
+}
---
+++ src/Games/BrainCalc.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BrainGames\Games\BrainCalc;
+
+use function BrainGames\Engine\runGame;
+
+function getRandomExpression(): array
+{
+    $operations = ['+', '-', '*'];
+    $num1 = rand(1, 20);
+    $num2 = rand(1, 20);
+    $operation = $operations[array_rand($operations)];
+
+    $expression = "$num1 $operation $num2";
+
+    switch ($operation) {
+        case '+':
+            $result = $num1 + $num2;
+            break;
+        case '-':
+            $result = $num1 - $num2;
+            break;
+        case '*':
+            $result = $num1 * $num2;
+            break;
+        default:
+            throw new \\Exception("Unknown operation: {$operation}");
+    }
+
+    return ['expression' => $expression, 'result' => (string)$result];
+}
