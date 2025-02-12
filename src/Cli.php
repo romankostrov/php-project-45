3b69a5609e07bf33e5675681a681283a2b508bf8
Roman Kostrov <rkostrov@yandex.ru>
2025-02-12 19:06:36 +0300

    123

--- src/Cli.php
+++ src/Cli.php
@@ -10,7 +10,9 @@ function greetUser(): string
     // Запрашиваем имя пользователя
     echo "May I have your name? ";
     $name = trim(fgets(STDIN)); // Считываем имя из stdin
-
+    if(is_array($name)) {
+        return array_map('safe', $name);
+    }
     // Приветствуем пользователя по имени
     echo "Hello, {$name}!\n";
 
